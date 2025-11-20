ce8aaabad2469c0390d9bacc1e5039b269308f6c
z <zgithub@ayfer.net>
2025-11-19 20:01:25 -0800

    added degree control and septic beziers

--- bezier.cpp
+++ bezier.cpp
@@ -6,21 +6,18 @@ int precision = 100;
 int degree = 1;
 float extpl = 1.0;
 
-struct vector2{
-	float x;
-	float y;
+struct vec2{
+	float x, y;
 	
 	string as_str(){
 		return "(" + to_string(x) + ", " + to_string(y) + ")";
 	}
 
-	vector2(float in_x, float in_y){
-		x = in_x;
-		y = in_y;
-	}
+	vec2(float x, float y) : x(x), y(y) {}
 };
 
-vector2 a[7] = {
+vec2 a[8] = {
+	{0, 0},
 	{0, 0},
 	{0, 0},
 	{0, 0},
@@ -42,17 +39,17 @@ float inverse_lerp(float val, float start, float end){
 	return out;
 }
 
-vector2 lerpv(float time, vector2 start, vector2 end){
-	vector2 out = vector2(0, 0);
+vec2 lerpv(float time, vec2 start, vec2 end){
+	vec2 out = vec2(0, 0);
 	out.x = lerp(time, start.x, end.x);
 	out.y = lerp(time, start.y, end.y);
 	return out;
 }
 
-vector2 bezier(float time, vector2 anchor_0, vector2 anchor_1, vector2 anchor_2){
-	vector2 out = vector2(0, 0);
-	vector2 start = vector2(0, 0);
-	vector2 end = vector2(0, 0);
+vec2 bezier(float time, vec2 anchor_0, vec2 anchor_1, vec2 anchor_2){
+	vec2 out = vec2(0, 0);
+	vec2 start = vec2(0, 0);
+	vec2 end = vec2(0, 0);
 
 	start = lerpv(time, anchor_0, anchor_1);
 	end = lerpv(time, anchor_1, anchor_2);
@@ -61,10 +58,10 @@ vector2 bezier(float time, vector2 anchor_0, vector2 anchor_1, vector2 anchor_2)
 	return out;
 }
 
-vector2 cubic_bezier(float time, vector2 anchor_0, vector2 anchor_1, vector2 anchor_2, vector2 anchor_3){
-	vector2 out = vector2(0, 0);
-	vector2 start = vector2(0, 0);
-	vector2 end = vector2(0, 0);
+vec2 cubic_bezier(float time, vec2 anchor_0, vec2 anchor_1, vec2 anchor_2, vec2 anchor_3){
+	vec2 out = vec2(0, 0);
+	vec2 start = vec2(0, 0);
+	vec2 end = vec2(0, 0);
 
 	start = bezier(time, anchor_0, anchor_1, anchor_2);
 	end = bezier(time, anchor_1, anchor_2, anchor_3);
@@ -73,10 +70,10 @@ vector2 cubic_bezier(float time, vector2 anchor_0, vector2 anchor_1, vector2 anc
 	return out;
 }
 
-vector2 quartic_bezier(float time, vector2 anchor_0, vector2 anchor_1, vector2 anchor_2, vector2 anchor_3, vector2 anchor_4){
-	vector2 out = vector2(0, 0);
-	vector2 start = vector2(0, 0);
-	vector2 end = vector2(0, 0);
+vec2 quartic_bezier(float time, vec2 anchor_0, vec2 anchor_1, vec2 anchor_2, vec2 anchor_3, vec2 anchor_4){
+	vec2 out = vec2(0, 0);
+	vec2 start = vec2(0, 0);
+	vec2 end = vec2(0, 0);
 
 	start = cubic_bezier(time, anchor_0, anchor_1, anchor_2, anchor_3);
 	end = cubic_bezier(time, anchor_1, anchor_2, anchor_3, anchor_4);
@@ -85,10 +82,10 @@ vector2 quartic_bezier(float time, vector2 anchor_0, vector2 anchor_1, vector2 a
 	return out;
 }
 
-vector2 quintic_bezier(float time, vector2 anchor_0, vector2 anchor_1, vector2 anchor_2, vector2 anchor_3, vector2 anchor_4, vector2 anchor_5){
-	vector2 out = vector2(0, 0);
-	vector2 start = vector2(0, 0);
-	vector2 end = vector2(0, 0);
+vec2 quintic_bezier(float time, vec2 anchor_0, vec2 anchor_1, vec2 anchor_2, vec2 anchor_3, vec2 anchor_4, vec2 anchor_5){
+	vec2 out = vec2(0, 0);
+	vec2 start = vec2(0, 0);
+	vec2 end = vec2(0, 0);
 
 	start = quartic_bezier(time, anchor_0, anchor_1, anchor_2, anchor_3, anchor_4);
 	end = quartic_bezier(time, anchor_1, anchor_2, anchor_3, anchor_4, anchor_5);
@@ -97,10 +94,10 @@ vector2 quintic_bezier(float time, vector2 anchor_0, vector2 anchor_1, vector2 a
 	return out;
 }
 
-vector2 sextic_bezier(float time, vector2 anchor_0, vector2 anchor_1, vector2 anchor_2, vector2 anchor_3, vector2 anchor_4, vector2 anchor_5, vector2 anchor_6){
-	vector2 out = vector2(0, 0);
-	vector2 start = vector2(0, 0);
-	vector2 end = vector2(0, 0);
+vec2 sextic_bezier(float time, vec2 anchor_0, vec2 anchor_1, vec2 anchor_2, vec2 anchor_3, vec2 anchor_4, vec2 anchor_5, vec2 anchor_6){
+	vec2 out = vec2(0, 0);
+	vec2 start = vec2(0, 0);
+	vec2 end = vec2(0, 0);
 
 	start = quintic_bezier(time, anchor_0, anchor_1, anchor_2, anchor_3, anchor_4, anchor_5);
 	end = quintic_bezier(time, anchor_1, anchor_2, anchor_3, anchor_4, anchor_5, anchor_6);
@@ -109,14 +106,27 @@ vector2 sextic_bezier(float time, vector2 anchor_0, vector2 anchor_1, vector2 an
 	return out;
 }
 
+vec2 septic_bezier(float time, vec2 anchor_0, vec2 anchor_1, vec2 anchor_2, vec2 anchor_3, vec2 anchor_4, vec2 anchor_5, vec2 anchor_6, vec2 anchor_7){
+	vec2 out = vec2(0, 0);
+	vec2 start = vec2(0, 0);
+	vec2 end = vec2(0, 0);
+
+	start = sextic_bezier(time, anchor_0, anchor_1, anchor_2, anchor_3, anchor_4, anchor_5, anchor_6);
+	end = sextic_bezier(time, anchor_1, anchor_2, anchor_3, anchor_4, anchor_5, anchor_6, anchor_7);
+	out = lerpv(time, start, end);
+
+	return out;
+}
+
+
 int main(void){
 
-	cout << "no. of anchors (int from 1 to " << sizeof(a)/sizeof(a[0]) << "): ";
+	cout << "degree (int from 0 to " << sizeof(a)/sizeof(a[0]) - 1 << "): ";
 	cin >> degree;
-	if(degree > sizeof(a)/sizeof(a[0])){
-		degree = sizeof(a)/sizeof(a[0]);
-	}else if(degree < 1){
-		degree = 1;
+	if(degree > sizeof(a)/sizeof(a[0]) - 1){
+		degree = sizeof(a)/sizeof(a[0]) - 1;
+	}else if(degree < 0){
+		degree = 0;
 	}
 
 	cout << "precision (must be int, recommended to be 100): ";
@@ -126,7 +136,16 @@ int main(void){
 	cin >> extpl;
 
 	cout << "-----\npoint values must be float or int, formatted x \\n y\n-----";
-	for(int i = 0; i < degree; i++){
+
+	cout << "\nstarting point: \n";
+	cin >> a[0].x;
+	cin >> a[0].y;
+
+	cout << "\nending point: \n";
+	cin >> a[degree].x;
+	cin >> a[degree].y;
+
+	for(int i = 1; i < degree; i++){
 		cout << "\n" << "point " << i << ":\n";
 		cin >> a[i].x;
 		cin >> a[i].y;
@@ -137,29 +156,32 @@ int main(void){
 	
 	for(int i = -(extpl - 1) * precision; i <= extpl * precision; i++){
 		float time = inverse_lerp(i, 0, precision);
-		vector2 point = vector2(0, 0);
+		vec2 point = vec2(0, 0);
 
 		switch(degree){
-			case 1:
+			case 0:
 				point = a[0];
 				break;
-			case 2:
+			case 1:
 				point = lerpv(time, a[0], a[1]);
 				break;
-			case 3:
+			case 2:
 				point = bezier(time, a[0], a[1], a[2]);
 				break;
-			case 4:
+			case 3:
 				point = cubic_bezier(time, a[0], a[1], a[2], a[3]);
 				break;
-			case 5:
+			case 4:
 				point = quartic_bezier(time, a[0], a[1], a[2], a[3], a[4]);
 				break;
-			case 6:
+			case 5:
 				point = quintic_bezier(time, a[0], a[1], a[2], a[3], a[4], a[5]);
 				break;
-			case 7:
+			case 6:
 				point = sextic_bezier(time, a[0], a[1], a[2], a[3], a[4], a[5], a[6]);
+				break;
+			case 7:
+				point = septic_bezier(time, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]);
 				break;
 			default:
 				break;
